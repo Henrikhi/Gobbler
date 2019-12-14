2ef241211cc48ae57e8c0b4f1c0b72d8b3ee4480
Henrik <henrik.hirvonen77@gmail.com>
2019-12-14 19:55:31 +0200

    testi

--- src/main/java/gobbler/config/CustomUserDetailsService.java
+++ src/main/java/gobbler/config/CustomUserDetailsService.java
@@ -1,9 +1,6 @@
 package gobbler.config;
 
-import gobbler.domain.Follow;
 import gobbler.domain.Gobbler;
-import gobbler.domain.Picture;
-import gobbler.repositories.FollowRepository;
 import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -12,12 +9,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import gobbler.repositories.GobblerRepository;
-import gobbler.repositories.PictureRepository;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.time.LocalDateTime;
-import javax.annotation.PostConstruct;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
